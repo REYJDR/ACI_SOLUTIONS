6625ae7ec9ea88f70d5f1780dd6300ae60cbcda4
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-01-25 12:07:06 -0600

    update

---
+++ SftpFilesSync/SftpConnetion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SftpFilesSync
+{
+    class SftpConnection
+    {
+        SftpParam param = new SftpParam();
+        
+        public  void StartConn()
+        {
+
+   
+
+            try
+            {
+
+              
+               
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+
+
+            }
+
+           
+        }
+
+        public void Closed()
+        {
+
+           
+
+        }
+
+ 
+               
+
+    }
+
+ }
---
+++ SftpFilesSync/SftpParam.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SftpFilesSync
+{
+    class SftpParam
+    {
+
+        private string hostaname;
+        private string port;
+        private string user;
+        private string password;
+
+        public string Hostaname { get => hostaname; set => hostaname = value; }
+        public string Port { get => port; set => port = value; }
+        public string User { get => user; set => user = value; }
+        public string Password { get => password; set => password = value; }
+
+
+        public void SetValueOnFile()
+        {
+            File.WriteAllText("SftpParams.txt", string.Empty);
+
+            TextWriter file = new StreamWriter("SftpParams.txt");
+
+            // write lines of text to the file
+            file.WriteLine(Hostaname);
+            file.WriteLine(port);
+            file.WriteLine(User);
+            file.WriteLine(Password);
+
+            // close the stream     
+            file.Close();
+
+        }
+
+        public void GetValueFromFile()
+        {
+
+            if (File.Exists("SftpParams.txt"))
+            {
+                // create reader & open file
+                TextReader file = new StreamReader("SftpParams.txt");
+
+                Hostaname = file.ReadLine();
+                port = file.ReadLine();
+                User = file.ReadLine();
+                Password = file.ReadLine();
+
+
+                // close the stream
+                file.Close();
+            }
+
+        }
+
+        public string ConString()
+        {
+            string strConn = "";
+
+            // create reader & open file
+            if (File.Exists("SftpParams.txt"))
+            {
+
+                TextReader file = new StreamReader("SftpParams.txt");
+
+                Hostaname = file.ReadLine();
+                port = file.ReadLine();
+                User = file.ReadLine();
+                Password = file.ReadLine();
+
+                strConn = "";
+
+                // close the stream
+                file.Close();
+
+
+            }
+
+
+            return strConn;
+        }
+
+
+
+
+    }
+}
